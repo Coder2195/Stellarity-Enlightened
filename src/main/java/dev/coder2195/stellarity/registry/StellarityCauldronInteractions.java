c4619b54c8a822f3b317efa1dd0295c3bb3ee57c
Coder2195 <coder2195mail@gmail.com>
2026-09-14 10:13:49 -0400

    ✨ cauldron interactions

--- src/main/java/dev/coder2195/stellarity/registry/StellarityCauldronInteractions.java
+++ src/main/java/dev/coder2195/stellarity/registry/StellarityCauldronInteractions.java
@@ -5,15 +5,19 @@ import dev.coder2195.stellarity.mixin.accessor.CauldronInteractionsDispatcherAcc
 import net.minecraft.core.cauldron.CauldronInteraction;
 import net.minecraft.core.cauldron.CauldronInteractions;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.Block;
 
 public interface StellarityCauldronInteractions {
 	CauldronInteraction.Dispatcher DRAGON_BREATH = CauldronInteractionsAccessor.newDispatcher("stellarity:dragon_breath");
 	static void init() {
 		((CauldronInteractionsDispatcherAccessor) CauldronInteractions.WATER).stellarity$put(Items.DRAGON_BREATH, (state, level, pos, player, hand, itemInHand) -> {
 			if (level.isClientSide()) return InteractionResult.SUCCESS;
-			
-			
+			level.setBlock(pos, StellarityBlocks.DRAGON_BREATH_CAULDRON.withPropertiesOf(state), Block.UPDATE_ALL);
+			itemInHand.consume(1, player);
+			player.getInventory().add(new ItemStack(Items.GLASS_BOTTLE));
+
 			return InteractionResult.SUCCESS_SERVER;
 		});
 	}
